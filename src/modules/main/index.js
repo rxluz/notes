228f917da732064176aea07c5bfe79bbd9ce1068
Ricardo Luz <hi@rxluz.dev>
2020-04-26 11:44:44 +0100

    refact(basic-packages): put routes inside common folder

--- src/modules/main/index.js
+++ src/modules/main/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Main.scss'
 import 'react-tippy/dist/tippy.css'
 
-import RoutesList from './RoutesList'
+import RoutesList from '../../common/routes/RoutesList'
 
-const Main = () => (
-  <div className="App">
-    <RoutesList />
-  </div>
-)
+const Main = () => {
+  return (
+    <div className="App">
+      <RoutesList />
+    </div>
+  )
+}
 
 export default Main
