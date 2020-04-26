22f53a2cdb61e7198be909ef490185bad850ecc2
Ricardo Luz <hi@rxluz.dev>
2020-04-26 13:14:50 +0100

    feat(basic-packages): introduce 404 page

--- src/common/routes/RoutesList.js
+++ src/common/routes/RoutesList.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Switch, Route } from 'react-router-dom'
 import TestRoute from './TestRoute'
+import NotFound from '../../modules/main/NotFound'
 
 const RoutesList = () => (
   <Switch>
@@ -19,7 +20,7 @@ const RoutesList = () => (
     </Route>
 
     <Route path="*" exact={true}>
-      <TestRoute title="not found" />
+      <NotFound />
     </Route>
   </Switch>
 )
